3b2eea227c1badb1303d69449ab54773d1ef2917
Alejo Borracci <borraccialejo@hotmail.es>
2024-04-28 02:45:38 -0300

    render video

--- src/Components/videoplayer/videoplayer.jsx
+++ src/Components/videoplayer/videoplayer.jsx
@@ -1,8 +1,21 @@
-import styles from './video.module.css'
+'use client';
 
+import styles from './video.module.css';
+import ReactPlayer from 'react-player/youtube';
 
 export default function VideoPlayer() {
-    return (
-      <div className={styles.video}>videoplayer</div>
-    )
+
+  const videoURL = "https://www.youtube.com/watch?v=fLzU21ltH4U";
+  return (
+    <div className={styles.container}>
+      <div className={styles.video}>
+        <ReactPlayer
+          url={videoURL}
+          controls={true}
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </div>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,14 @@
 import Searchbar from '@/Components/search/search';
 import styles from './page.module.css'
-
+import VideoPlayer from '@/Components/videoplayer/videoplayer';
 
 export default function MyApp() {
-
   return (
-        <div className={styles.container}>
-          APP
-        </div>
+    <div className={styles.container}>
+      <div className={styles.video}>
+        <VideoPlayer/>
+      </div>
+    </div>
   );
 }
 
